5d293e93cd85d9c59fe6a4bed0fc11dd02bb38af
BojanStupar089 <bojanstupar089@gmail.com>
2025-10-05 01:51:22 +0200

    init.

---
+++ ProntoWebApiS/ProntoWebApiP/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProntoWebApiP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Dje ste Skaljari!!");
+        }
+    }
+}
